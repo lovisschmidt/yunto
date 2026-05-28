cac530000a367a35e8e4dccff4d5a4e7574918fd
Lovis Schmidt <lovis.schmidt.dev@gmail.com>
2026-05-28 13:08:52 +0200

    chore: update version, fix expo version syncing

--- app.config.js
+++ app.config.js
@@ -1,8 +1,11 @@
+const { version } = require("./package.json");
+
 const IS_DEV = process.env.APP_ENV === "development";
 
 /** @param {{ config: import('@expo/config-types').ExpoConfig }} ctx */
 module.exports = ({ config }) => ({
   ...config,
+  version,
   android: {
     ...config.android,
     package: IS_DEV ? "com.yunto.app.debug" : "com.yunto.app",
